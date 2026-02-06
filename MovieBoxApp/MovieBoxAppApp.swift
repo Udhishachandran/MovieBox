5c508d22e2a06bdd8691beca2e3a77d050276605
Udhisha C <udhisha.c@travancoreanalytics.com>
2026-02-06 19:28:49 +0530

    Initial Commit

---
+++ MovieBoxApp/MovieBoxAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MovieBoxAppApp.swift
+//  MovieBoxApp
+//
+//  Created by Udhisha C on 06/02/26.
+//
+
+import SwiftUI
+
+@main
+struct MovieBoxAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
